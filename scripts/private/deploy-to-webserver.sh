bd2d9deba8239c0ce9ac02e986752c279a2967b3
vitor <rotiv695@gmail.com>
2017-09-01 14:03:00 +0100

    change made to script again

--- scripts/private/deploy-to-webserver.sh
+++ scripts/private/deploy-to-webserver.sh
@@ -2,7 +2,7 @@
 
 ssh ubuntu@54.194.13.84
 sudo su
-cd var/www/html
+cd /var/www/html
 apt-get update
 apt-get install build-essential libssl-dev
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
@@ -12,7 +12,7 @@ exit
 exit
 ssh ubuntu@54.194.13.84
 sudo su
-cd /var/www/html/
+cd /var/www/html
 nvm install 6.10.3
 nvm use 6.10.3
 apt-get install npm
